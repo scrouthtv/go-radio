deb9105991d30973333f2f4e46766518a3e47a58
Lenni vH <lennivh24@gmail.com>
2020-11-18 14:53:50 +0100

    added dependency to word-wrap lib

--- src/main.go
+++ src/main.go
@@ -49,6 +49,7 @@ func main() {
 			os.Exit(1)
 		} else {
 			fmt.Println("got", len(events), "events:")
+			tui.SortEventsByStart(events)
 			var i int
 			var ev stations.Event
 			for i, ev = range events {
@@ -62,6 +63,21 @@ func main() {
 				}
 			}
 		}
+	/*case "splittest":
+			const msg string = `Weniger Einnahmen, mehr Zuspruch?
+	Die Kirchen in der Corona-Krise
+	Am Mikrofon: Michael Roehl
+
+	Gäste:
+	Dr. Johann Hinrich Claussen, Kulturbeauftragter des Rates der EKD
+	Rainer Schießler, Pfarrer, Pfarrei St. Maximilian München, München
+	Corinna Zisselsberger, Pfarrerin, Evangelische Kirchengemeinde St. Petri-St. Marien, Berlin
+	Dr. Christiane Florin, Deutschlandfunk-Redakteurin für Religion und Gesellschaft und freie Autorin
+
+	Hörertel.: 00800 - 4464 4464
+	laenderzeit@deutschlandfunk.deDie großen deutschen Kirchen leiden unter einem eklatanten Mitgliederschwund. Weit über zweihunderttausend Katholiken und ebenso viele Protestanten erklärten im letzten Jahr ihren Kirchenaustritt. Und der Trend hält an. Bis zum Jahr 2060 könnte sich die Zahl der Gläubigen sogar halbieren, schätzen Wissenschaftler. Ein riesiges Problem, das die Kirchen mittel- und langfristig in ihrer Existenz bedroht. Und nun sorgt auch noch die Corona-Pandemie für große Herausforderungen. Gottesdienste dürfen nur noch unter Sicherheitsauflagen und mit stark verminderter Besucherzahl durchgeführt werden. Oft wurden sie komplett durch Videoübertragungen aus den Gotteshäusern ersetzt. Gemeindearbeit und Seelsorge sind stark beeinträchtigt. Doch gerade in Krisenzeiten ist geistlicher Zuspruch gefragt und viele Gläubige fühlen sich vernachlässigt und im Stich gelassen. Mit innovativen Ideen versuchen manche Kirchenleute den Kontakt zu den Menschen in ihrer Gemeinde dennoch aufrecht zu erhalten.`
+			const width int = 10
+			var lines []string = tui.Softwrap*/
 	default:
 		fmt.Println("Unknown option", os.Args[1])
 	}
